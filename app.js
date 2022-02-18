01bdeaf8c380821f361c3d1d2941906a6877e534
unknown <pankaj11koundal@gmail.com>
2022-02-18 20:52:21 +0530

    Databases added

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
+// Importing or requiring module files: express, bodyparser, lodash, mongoose.
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 const _ = require("lodash");
-const postItems = [];
+const mongoose = require("mongoose");
+const { type } = require("express/lib/response");
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
 
-app.set('view engine', 'ejs');
+// Eastablishing connection with mongodb localhost
+mongoose.connect("mongodb://localhost:27017/Personal-Blog-Database").then(() => {
+  console.log("connection successfull");
+}).catch((err) => {
+  console.log(err);
+});
+// Creating the schema for blogs
+const blogSchema = new mongoose.Schema({ title: String, description: String });
+const homeSchema = new mongoose.Schema({ description: String })
+// Creating the collection or model required
+const blogModel = new mongoose.model("blog", blogSchema);
+const homeModel = new mongoose.model("home", homeSchema);
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+var postItems = [];
 
 // Home routings or root routings
-app.get("/", (req, res) => {
-  res.render("home", {startingContent: homeStartingContent, content:postItems});
+app.get("/", (req, res) => {  
+  
+  console.log("Executed first");
+  blogModel.find((err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(docs);
+      res.render("home", {startingContent: homeStartingContent, content: docs});
+    }
+  });
+  
+  // console.log(blogItems);
 })
 
 // About page routings
@@ -35,12 +62,14 @@ app.get("/compose", (req, res) => {
 })
 
 app.post("/compose", (req,res) => {
-  const post = {
-    postTitle: req.body.postTitle,
-    postContent: req.body.postContent 
-  }
-
-  postItems.push(post);
+  const blog = new blogModel({
+    title: req.body.postTitle,
+    description: req.body.postContent 
+  })
+  blog.save(err => {
+    if (!err) 
+      console.log("Post saved successfully");
+  });
   res.redirect("/");
 })
 
@@ -48,7 +77,7 @@ app.post("/compose", (req,res) => {
 app.get("/posts/:parameters", (req, res) => {
   const parameters = _.lowerCase(req.params.parameters);
 
-  postItems.forEach(element => { 
+  blogItems.forEach(element => { 
     const titleLower = _.lowerCase(element.postTitle)
     if (titleLower === parameters){
       res.render("post", {postHeading: element.postTitle, postContent: element.postContent});
@@ -56,16 +85,6 @@ app.get("/posts/:parameters", (req, res) => {
   });
 
 })
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
